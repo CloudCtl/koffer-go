4bb1cecfbbc9f6dcc5b97845ae132ab63cc1966d
usrbinkat <usrbinkat@braincraft.io>
2020-08-02 23:58:43 -0600

    update automation

---
+++ dev.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+# cobra init --pkg-name github.com/CodeSparta/koffer-go
+# cobra add mirror
+# cobra add bundle
+# go build
+# gitup devel
+
+goCmd=$(which go)
+
+rm /bin/koffer 2>/dev/null
+rm -rf /root/koffer 2>/dev/null
+mkdir -p /tmp/bin
+
+git stage -A; git commit -m 'testing'; git push origin master
+
+plugins="
+    "github.com/spf13/cobra" \
+    "github.com/go-git/go-git" \
+    "github.com/go-git/go-git/plumbing" \
+    "github.com/CodeSparta/koffer-go/plugins/err" \
+    "github.com/CodeSparta/koffer-go/plugins/log" \
+    "github.com/CodeSparta/koffer-go/plugins/auth" \
+"
+for i in ${plugins}; do
+  ${goCmd} get -u ${i};
+done
+
+${goCmd} build
+
+mv ./dev koffer 2>/dev/null
+cp -f koffer /usr/bin/koffer 2>/dev/null
+mv -f koffer /tmp/bin/koffer 2>/dev/null
+
