92f2eca01c2db083fb1256ade664787fd4ae328a
usrbinkat <usrbinkat@braincraft.io>
2020-08-03 02:50:26 +0000

    testing

--- cmd/bundle.go
+++ cmd/bundle.go
@@ -16,10 +16,20 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
+    "os"
+    "io"
+    "log"
+    "fmt"
+    "flag"
+    "sync"
+    "os/exec"
+    "strings"
 
-	"github.com/spf13/cobra"
-	"github.com/CodeSparta/koffer-go/plugins"
+    "github.com/spf13/cobra"
+    "github.com/go-git/go-git"
+    "github.com/go-git/go-git/plumbing"
+    "github.com/CodeSparta/koffer-go/plugins"
+//  "github.com/codesparta/koffer/entrypoint/src"
 )
 
 // bundleCmd represents the bundle command
@@ -36,7 +46,8 @@ Koffer Engine Bundle:
   engine and artifacts served via the CloudCtl delivery pod.
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("bundle called")
+		fmt.Println("Running Koffer Bundle....")
+		core()
 	},
 }
 
@@ -51,5 +62,102 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	bundleCmd.Flags().BoolP("help", "h", true, "koffer bundle help")
+	bundleCmd.Flags().BoolP("help", "h", false, "koffer bundle help")
+}
+
+func core() {
+
+    svcGit := flag.String("git", "github.com", "Git Server")
+    orgGit := flag.String("org", "CodeSparta", "Repo Owner/path")
+    repoGit := flag.String("repo", "collector-infra", "Plugin Repo Name")
+    branchGit := flag.String("branch", "master", "Git Branch")
+    pathClone := flag.String("dir", "/root/koffer", "Clone Path")
+
+    flag.Parse()
+
+    pullsecret.promptReqQuay()
+    pullsecret.writeConfig()
+
+    // build url from vars
+    gitslice := []string{ "https://", *svcGit, "/", *orgGit, "/", *repoGit }
+    url := strings.Join(gitslice, "")
+
+    // set branch
+    branchslice := []string{ "refs/heads/", *branchGit }
+    branch := strings.Join(branchslice, "")
+
+    runvars := "\n" +
+               "   Service: " + *svcGit + "\n" +
+               "  Org/Path: " + *orgGit + "\n" +
+               "      Repo: " + *repoGit + "\n" +
+               "    Branch: " + *branchGit + "\n" +
+               "      Path: " + *pathClone + "\n" +
+               "       URL: " + url + "\n" +
+               "       CMD: git clone " + url + *pathClone + "\n"
+    status.Info(runvars)
+
+    // Clone the given repository to the given directory
+    status.Info("git clone %s %s", url, *pathClone)
+
+    r, err := git.PlainClone(*pathClone, false, &git.CloneOptions{
+        URL:               url,
+        RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+	ReferenceName:     plumbing.ReferenceName(branch),
+	SingleBranch:      true,
+	Tags:              git.NoTags,
+    })
+    sanity.CheckIfError(err)
+    // ... retrieving the branch being pointed by HEAD
+    ref, err := r.Head()
+    sanity.CheckIfError(err)
+
+    // ... retrieving the commit object
+    commit, err := r.CommitObject(ref.Hash())
+    sanity.CheckIfError(err)
+
+    fmt.Println(commit)
+
+    registry := exec.Command("/usr/bin/run_registry.sh")
+    err = registry.Start()
+    if err != nil {
+        log.Fatal(err)
+    }
+    err = registry.Wait()
+
+    cmd := exec.Command("./site.yml")
+
+    var stdout, stderr []byte
+    var errStdout, errStderr error
+    stdoutIn, _ := cmd.StdoutPipe()
+    stderrIn, _ := cmd.StderrPipe()
+    err = cmd.Start()
+    if err != nil {
+        log.Fatalf("cmd.Start() failed with '%s'\n", err)
+    }
+
+    var wg sync.WaitGroup
+    wg.Add(1)
+    go func() {
+        stdout, errStdout = status.copyAndCapture(os.Stdout, stdoutIn)
+        wg.Done()
+    }()
+
+    stderr, errStderr = status.copyAndCapture(os.Stderr, stderrIn)
+
+    wg.Wait()
+
+    err = cmd.Wait()
+    if err != nil {
+        log.Fatalf("cmd.Run() failed with %s\n", err)
+    }
+    if errStdout != nil || errStderr != nil {
+        log.Fatal("failed to capture stdout \n")
+    }
+
+    errStr := string(stderr)
+    //outStr, errStr := string(stdout), string(stderr)
+    //fmt.Printf("\nout:\n%s\n", outStr)
+    if stderr != nil {
+        fmt.Printf("\nerr:\n%s\n", errStr)
+    }
 }
