30228ace2009f153adfb523d8a4bc473ebfd5d28
usrbinkat <usrbinkat@braincraft.io>
2020-08-03 00:01:27 -0600

    automation

---
+++ auto.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+sudo /usr/bin/podman run \
+    -it --rm --name go-build \
+    --volume $(pwd)/bin:/tmp/bin:z \
+    --entrypoint /root/dev/build.sh \
+    --volume $(pwd):/root/dev:z \
+  docker.io/ocpredshift/red-gotools
+    
