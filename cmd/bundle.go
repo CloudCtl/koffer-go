5db2661f23993ad670fd16748128a7eec6296248
usrbinkat <usrbinkat@braincraft.io>
2020-08-03 03:54:32 +0000

    testing

--- cmd/bundle.go
+++ cmd/bundle.go
@@ -71,11 +71,11 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	bundleCmd.Flags().BoolP("help", "h", false, "koffer bundle help")
-	bundleCmd.Flags().StringVarP("service", "s", "github.com", "Git Server")
-	bundleCmd.Flags().StringVarP("user", "u", "CodeSparta", "Repo {User,Organization}/path")
-        bundleCmd.Flags().StringVarP("repo", "r", "collector-infra", "Plugin Repo Name")
-        bundleCmd.Flags().StringVarP("branch", "b", "master", "Git Branch")
-        bundleCmd.Flags().StringVarP("dir", "d", "/root/koffer", "Clone Path")
+	bundleCmd.Flags().StringVarP(service "service", "s", "github.com", "Git Server")
+	bundleCmd.Flags().StringVarP(user "user", "u", "CodeSparta", "Repo {User,Organization}/path")
+        bundleCmd.Flags().StringVarP(repo "repo", "r", "collector-infra", "Plugin Repo Name")
+        bundleCmd.Flags().StringVarP(branch "branch", "b", "master", "Git Branch")
+        bundleCmd.Flags().StringVarP(dir "dir", "d", "/root/koffer", "Clone Path")
 }
 
 func core() {
