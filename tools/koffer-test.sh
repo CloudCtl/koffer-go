74c29ccabb1a9d5ad32413a98d261fcfc4a86fcc
usrbinkat <usrbinkat@braincraft.io>
2020-08-19 15:01:55 -0600

    update koffer test

--- tools/koffer-test.sh
+++ tools/koffer-test.sh
@@ -8,7 +8,9 @@ mkdir -p /tmp/bundle ; \
 
 # Run koffer
 run_test () {
+run_home=${HOME}
 sudo podman run -it --rm --pull always \
+    --volume ${run_home}/.docker:/root/.docker:z \
     --volume /tmp/bundle:/root/deploy/bundle:z \
     --volume $(pwd)/bin/koffer:/usr/bin/koffer:z \
   docker.io/codesparta/koffer bundle $@
