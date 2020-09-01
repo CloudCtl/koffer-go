55bdef58f48db0eb4eed3827f67b371c54e1f955
usrbinkat <usrbinkat@braincraft.io>
2020-09-01 23:52:20 +0000

    testing validation

--- build.sh
+++ build.sh
@@ -6,10 +6,11 @@ rm -rf /root/koffer 2>/dev/null
 mkdir -p /tmp/bin
 
 ${goCmd} mod download
-
 ${goCmd} build -o bin/koffer
 
 mv ./dev ./bin/koffer 2>/dev/null
 cp -f ./bin/koffer /usr/bin/koffer 2>/dev/null
 cp -f ./bin/koffer /tmp/bin/koffer 2>/dev/null
-
+exit_code="$?"
+echo "exit code: ${exit_code}"
+exit 0
